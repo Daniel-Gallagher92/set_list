e3de61c8e15a322e2d889406e31de210dbd266ac
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-07-05 21:29:43 -0600

    Test: adds test for songs index

---
+++ spec/features/songs/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails-helper'
+
+RSpec.describe 'the songs index page' do 
+  #should this be merged with artist_songs_index?
+  
+  #As a user
+  #When I visit the songs index ("/songs")
+  #Then I see a header "All Songs"
+
+  describe "as a user" do 
+    describe "When I visit songs index (/songs)" do 
+      it "Then I see a header \"All Songs\" " do 
+        
+        
+        visit "/songs/" 
+        # save_and_open_page
+
+        expect(page).to have_content("All Songs")
+      end
+    end
+  end
+end
